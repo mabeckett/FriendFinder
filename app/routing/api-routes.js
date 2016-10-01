4a732794fdfa030bf91353e756006aef34398625
Andrew Beckett <Andrew Beckett>
2016-10-01 08:49:27 -0400

    push

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,11 +1,13 @@
+var friends = require('../data/friends.js');
+
 module.exports = function (app) {
 
 	app.get('/api/friends', function (req, res) {
-		// res.json();
+		res.json(friends);
 	});
 
 	app.post('/api/friends', function (req, res) {
 		
 	});
 
-}
+};
