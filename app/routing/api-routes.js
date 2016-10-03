22d3b1d16e067c8c94a22a18e3c1af4f04a49067
Andrew Beckett <Andrew Beckett>
2016-10-03 16:16:06 -0400

    push

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -7,7 +7,36 @@ module.exports = function (app) {
 	});
 
 	app.post('/api/friends', function (req, res) {
-		
-	});
 
+		var match = {
+			name: '',
+			photo: '',
+			friendDiff: 10
+		};
+
+		var userData = req.body;
+		var userName = userData.name;
+		var userPhoto = userData.photo;
+		var userScores = userData.scores;
+		//calculating difference
+		var totalDiff = 0;
+		//looping through friends 
+		for  (var i=0; i< friends.length; i++) {
+			console.log(friends[i].name);
+			totalDiff = 0;
+			//loop through all the scores of each friend
+			for (var f=0; f< friends[i].scores[f]; f++){
+				totalDiff += Math.abs(parseInt(userScores[f]) - parseInt(friends[i].scores[f]));
+
+				if (totalDiff <= match.friendDiff){
+					match.name = friends[i].name;
+					match.photo = friends[i].photo;
+					match.friendDiff = totalDiff;
+				};
+			};
+		};
+		//saving data and providing json
+		friends.push(userData);
+		res.json(match);
+	});
 };
